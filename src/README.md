ad92510ef31ee0bc39cde88106a3e5be93c8772b
Pratiyush Kumar Ray <pkray0021@gmail.com>
2023-07-23 22:27:26 +0530

    initial commit

---
+++ src/README.md
@@ -0,0 +1,31 @@
+1 Check Version of NPM , Node , Angular.
+2 Install NPM
+3 Create New Angular App - Configuration of Github back 1. Create Repository 2. Push Code on Github
+
+# ==============================================================
+
+- Add Headers
+  1. Create Components
+  2. Add HTML Code
+  3. Add CSS Code
+
+# ==============================================================
+
+- Make Model for Data handle
+
+  1. Create Food Model
+  2. Create data.ts (used for dummy data)
+
+- Add Image in assets folder -- Image will provide you in my github repository
+  1. Add Dummy Images
+- Create a Service for Create, Update, Delete and View Methods.
+- Create Home Component.
+
+# ==============================================================
+
+Complete design In home Component
+
+1. HTML Code
+2. TS Code 3. CSS Code
+
+- Install Star Rating
---
+++ src/data.ts
@@ -0,0 +1,78 @@
+import { Food } from "./app/shared/model/Food";
+
+export const sample_foods:Food[]=[
+    {
+        id:'1',
+        name:'spiceyc che',
+        cookTime:'10-20',
+        price:10,
+        favorite:false,
+        origins:['italy','france'],
+        stars:4.5,
+        imageUrl:'assets/food-1.jpg',
+        tags:['fastFodd','pizza','Lunch']
+
+    },
+    {
+        id:'2',
+        name:'spiceyc che',
+        cookTime:'10-20',
+        price:10,
+        favorite:false,
+        origins:['italy'],
+        stars:4.5,
+        imageUrl:'assets/food-1.jpg',
+        tags:['fastFodd','pizza','Lunch']
+
+    },
+    {
+        id:'3',
+        name:'spiceyc che',
+        cookTime:'10-20',
+        price:10,
+        favorite:false,
+        origins:['italy'],
+        stars:4.5,
+        imageUrl:'assets/food-1.jpg',
+        tags:['fastFodd','pizza','Lunch']
+
+    },
+    {
+        id:'4',
+        name:'spiceyc che',
+        cookTime:'10-20',
+        price:10,
+        favorite:false,
+        origins:['italy'],
+        stars:4.5,
+        imageUrl:'assets/food-1.jpg',
+        tags:['fastFodd','pizza','Lunch']
+
+    },
+    {
+        id:'5',
+        name:'spiceyc che',
+        cookTime:'10-20',
+        price:10,
+        favorite:false,
+        origins:['italy'],
+        stars:4.5,
+        imageUrl:'assets/food-1.jpg',
+        tags:['fastFodd','pizza','Lunch']
+
+    },
+    {
+        id:'6',
+        name:'spiceyc che',
+        cookTime:'10-20',
+        price:10,
+        favorite:false,
+        origins:['italy'],
+        stars:4.5,
+        imageUrl:'assets/food-1.jpg',
+        tags:['fastFodd','pizza','Lunch']
+
+    }
+
+
+]
